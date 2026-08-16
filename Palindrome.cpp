771998c12526c0537736e4899d6642588a502139
Ratan <kumarratan00941@gmail.com>
2026-08-16 21:09:18 +0530

    Create Palindrome.cpp

---
+++ Palindrome.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main () {
+	int n;
+	cin>>n;
+	
+	int original=n;
+	int reverse=0;
+	while(n>0) {
+		int digit=n%10;
+		reverse=reverse*10+digit;
+		n=n/10;
+	}
+	
+	if(original==reverse) {
+		cout<<"palindrome";
+	}
+	
+	else {
+		cout<<"not a palimdrome"
+	}
+}
